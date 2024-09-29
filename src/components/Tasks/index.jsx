f5cf521d7d394c66173ce2ba6850c53e071b9cd0
AlexAirCG <alexairggg@yandex.ru>
2024-09-29 06:56:54 +0300

    29.10.24

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -3,8 +3,18 @@ import "./tasks.css";
 import editSvg from "../../assets/img/edit.svg";
 import axios from "axios";
 import AddTasksForm from "./AddTasksForm";
+import Task from "./Task";
+import { Link } from "react-router-dom";
 
-const Tasks = ({ list, onEditTitle, onAddTask }) => {
+const Tasks = ({
+  list,
+  onEditTitle,
+  onAddTask,
+  onRemoveTask,
+  onEditTask,
+  onCompleteTask,
+  withoutEmpty,
+}) => {
   const editTitle = () => {
     const newTitle = window.prompt("Название списка", list.name);
     if (newTitle) {
@@ -21,40 +31,32 @@ const Tasks = ({ list, onEditTitle, onAddTask }) => {
 
   return (
     <div className="tasks">
-      <h2 className="tasks__title">
-        <span>{list.name}</span>
+      <Link to={`/lists/${list.id}`}>
+        {" "}
+        <h2 style={{ color: list.color.hex }} className="tasks__title">
+          {list.name}
 
-        <img onClick={editTitle} src={editSvg} alt="edit" />
-      </h2>
+          <img onClick={editTitle} src={editSvg} alt="edit" />
+        </h2>
+      </Link>
 
       <div className="tasks__item">
-        {!list.tasks.length && <h2>Задачи отсутствуют</h2>}
-        {list.tasks.map((task) => (
-          <div key={task.id} className="tasks__item-row">
-            <div className="checkbox">
-              <input id={`task-${task.id}`} type="checkbox" />
-              <label htmlFor={`task-${task.id}`}>
-                <svg
-                  width="13"
-                  height="10"
-                  viewBox="0 0 11 8"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M9.29999 1.20001L3.79999 6.70001L1.29999 4.20001"
-                    stroke="#bdb7b7"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
-              </label>
-            </div>
-            <input readOnly value={task.text} />
-          </div>
-        ))}
-        <AddTasksForm list={list} onAddTask={onAddTask} />
+        {!withoutEmpty && list.tasks && !list.tasks.length && (
+          <h2>Задачи отсутствуют</h2>
+        )}
+        {list.tasks &&
+          list.tasks.map((task) => (
+            <Task
+              key={task.id}
+              {...task}
+              list={list}
+              onEdit={onEditTask}
+              onRemove={onRemoveTask}
+              onComplete={onCompleteTask}
+            />
+          ))}
+
+        <AddTasksForm key={list.id} list={list} onAddTask={onAddTask} />
       </div>
     </div>
   );
